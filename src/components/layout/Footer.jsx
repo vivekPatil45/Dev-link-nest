3d8747eaa53801eb6519c3f1a4ab3f693813daeb
Vivek Patil <vivekrp4503@gmail.com>
2024-09-17 18:26:01 +0530

    footer completed

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Use react-router-dom for internal links
+
+export const Footer = () => {
+    return (
+        <footer className="z-10 flex w-full items-baseline justify-center rounded-lg text-center px-2 ">
+            <div className="text-sm leading-7 md:tracking-wide text-center text-black dark:text-theme-secondary-light">
+                <div>&copy; {new Date().getFullYear()} DevLinkNest</div>
+                <div className="lg:flex">
+                    <div className="mr-1 md:-mt-2 lg:mt-0">Developed by</div>
+                    <div className="md:-mt-2 lg:mt-0"></div>
+                    <a
+                        href="https://github.com/vivekPatil45"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="GitHub Profile of Vivek Patil"
+                        className="underline mr-1 duration-300 transition-all hover:text-theme-secondary"
+                    >
+                        Vivek Patil
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+};
--- src/components/layout/GeneralLayout.jsx
+++ src/components/layout/GeneralLayout.jsx
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import SideNavbar from '../sidenavbar/SideNavbar'
 import Header from './Header'
 import Sidebar from '../Sidebar'
 import MobileBottomNav from './MobileBottomNav'
+import { Footer } from './Footer'
+import { GlobalContext } from '../../contextAPI'
 
 const GeneralLayout = ({children}) => {
+
+    const { sidebar } = useContext(GlobalContext)
     return (
         <>
             <Header />
             <SideNavbar />
-            <div>
+            <div
+                className={`row-start-2 row-end-3 overflow-y-scroll h-full w-full bg-gray-100 dark:bg-[#101623] ${
+                    sidebar ? 'max-[1024px]:overflow-hidden' : ''
+                }`}
+            >
 
                 <nav>
                     <Sidebar />
                 </nav>
 
-                <main>
-                    <div>
+                <main
+                    data-custom="restrict-click-outside"
+                    className="flex flex-col min-h-full px-4 pb-2 md:ml-[290px] lg:w-[calc(100%-290px)] bg-white dark:bg-inherit"
+                    id="main"
+                >
+                    <div className="flex-grow">
                         {children}
                     </div>
 
                     <MobileBottomNav/>
+                    <Footer/>
                 </main>
             </div>
         </>
