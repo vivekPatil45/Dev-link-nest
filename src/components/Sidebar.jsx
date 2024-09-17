a8f811af90f02ebebbff38b2f3c41994c0bc48da
Vivek Patil <vivekrp4503@gmail.com>
2024-09-17 18:38:15 +0530

    complete the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Searchbar from './searchbar/Searchbar'
 
 const Sidebar = () => {
+    const inputRef = useRef(null)
     return (
-        <div>
-        
+        <div className="fixed bottom-0 left-0 z-30 w-[306px] h-[calc(100vh-78px)] hidden md:flex flex-col items-start px-6 py-4 tall:py-7 gap-4 tall:gap-10 bg-white dark:bg-slate-800 shadow-sidebar dark:shadow-none">
+            <div className="w-full flex flex-col gap-4 tall:gap-6">
+                <Searchbar
+                    inputRef={inputRef}
+                />
+            </div>
         </div>
     )
 }
---
+++ src/components/searchbar/Searchbar.jsx
@@ -0,0 +1,38 @@
+import React, { useRef } from 'react'
+import { Icons } from '../icons';
+
+const Searchbar = ({inputRef}) => {
+    const formRef = useRef(null);
+    return (
+        <form
+            data-custom="restrict-click-outside"
+            noValidate
+            role="search"
+            ref={formRef}
+        >   
+            <div className=' relative'>
+                <div className="relative w-full h-12 flex items-center justify-between  border border-theme-secondary/25 dark:border-none rounded-lg">
+                    <label htmlFor="simple-search" className="sr-only">
+                        Quick search
+                    </label>
+
+                    <input
+                        ref={inputRef}
+                        type="text"
+                        id="simple-search"
+                        name="simple-search"
+                        className="peer h-12 w-full flex items-center justify-start pl-[46px] pr-4 py-3 bg-slate-100 bg-opacity-50 dark:bg-zinc-400 dark:bg-opacity-20 rounded-[10px] outline-none hover:shadow-input-hover focus:shadow-input-focus dark:hover:shadow-input-hover-dark dark:focus:shadow-input-focus-dark"
+                        placeholder="Quick search..."
+                        
+                        autoComplete="off"
+                        required
+                    />
+
+                    <Icons.search className="h-5 w-5 absolute top-[14px] left-4 text-gray-400 peer-focus:text-primary dark:peer-focus:text-slate-100" />
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default Searchbar
